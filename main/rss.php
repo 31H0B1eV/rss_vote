038ebca3b0b0c4c05158066d94680adfb2fe90ae
Artem Zinoviev <arzinoviev@gmail.com>
2015-02-05 17:52:01 +0300

    add more feed sources

--- main/rss.php
+++ main/rss.php
@@ -1,9 +1,12 @@
 <?php
 
-$rss = "http://www.reddit.com/r/IndianaUniversity/new/.rss";
+$rssIndianaUniversity = "http://www.reddit.com/r/IndianaUniversity/new/.rss";
+$rssBloomington = "http://www.reddit.com/r/bloomington/new/.rss";
+$rssHeraldTimesOnline = "http://www.heraldtimesonline.com/search/?q=&t=article&l=100&d=&d1=&d2=&s=start_time&sd=desc&nsa=eedition&c[]=news/local,news/local/*&f=rss";
+$rssMagbloom = "http://www.magbloom.com/feed/";
 
-$xml = @simplexml_load_file($rss);
-if($xml===false)die('Error parse RSS: '.$rss);
+$xml = @simplexml_load_file($rssIndianaUniversity);
+if($xml===false)die('Error parse RSS: '.$rssIndianaUniversity);
 
 foreach($xml->xpath('//item') as $item){
     echo '<h3 style="text-align: center;color: darkcyan;">'.$item->title . '</h3>' . '<br />';
